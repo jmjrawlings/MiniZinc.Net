b890ee48263fd199a40ddef2806b3353b957beea
Justin Rawlings <hrkn@pm.me>
2023-05-02 20:05:32 +1000

    xd

---
+++ tests/MiniZinc.Net.Tests/Prelude.fs
@@ -0,0 +1,11 @@
+﻿namespace MiniZinc.Net.Tests
+
+open System.Runtime.CompilerServices
+open FluentAssertions
+
+[<Extension>]
+type Prelude() =
+    
+    [<Extension>]
+    static member ShouldEqual(a: obj, b: string) =
+        (string a).Should().Be(b, "")
---
+++ tests/MiniZinc.Net.Tests/SolveTests.fs
@@ -0,0 +1,46 @@
+﻿namespace MiniZinc.Net.Tests
+
+module SolveTests =
+
+    open MiniZinc
+    open FSharp.Control
+    open MiniZinc.Command
+    open Xunit
+    open FluentAssertions
+       
+    [<Fact>]
+    let ``test solve simple model`` () =
+        let model = Model.FromString(
+            """
+            var 0..10: x;
+            var 0..10: y;
+            constraint x < y;
+            constraint y < 3;
+            """ 
+            )
+        let result = MiniZinc.solve model
+        let stdout = result.Exec().Result.StdOut
+        stdout.Should().Contain("x","")
+
+    [<Fact>]
+    let ``test stream simple model`` () =
+        let model = Model.FromString(
+            """
+            var 0..10: x;
+            var 0..10: y;
+            constraint x < y;
+            constraint y < 3;
+            """
+            )
+        let sol =
+            model
+            |> MiniZinc.Stream
+            |> TaskSeq.choose (function
+                | CommandMessage.Output out -> Some out
+                | _ -> None
+                )
+            |> TaskSeq.last
+            
+        let msg = sol.Result.Text                    
+            
+        msg.Should().Contain("x","")
