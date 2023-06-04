4121a2f1ab63fad09bf9afe410879204af3978cf
Justin Rawlings <hrkn@pm.me>
2023-06-04 21:29:52 +1000

    encoding

--- src/MiniZinc.Model/Encode.fs
+++ src/MiniZinc.Model/Encode.fs
@@ -1,52 +1,103 @@
-﻿namespace MiniZinc
+﻿namespace MiniZinc.Encode
+open System
+open Microsoft.FSharp.Quotations
 open MiniZinc
 
-module Encode =
-     
-    let includeItem (x: IncludeItem) =
+type Encode =
+    
+    static member encode (x: IncludeItem) =
         string x
     
-    let enumItem (x: EnumItem) =
-        // { Name : Id
-        // ; Annotations : Annotations
-        // ; Cases : EnumCase list }
-        ""
-        
-    let enumCase (x: EnumCase) =
-        ""
+    static member encode (x: EnumItem) =
+        $"enum {Encode.encode x.Name}"
+                
+    static member encode (x: EnumCase) =
+        match x with
+        | Name x -> Encode.encode x
+        | Expr x -> Encode.encode x
     
-    let synonym (x: SynonymItem) =
+    static member encode (x: SynonymItem) =
+        match x with
+        | (id, anns, ti) ->
+            $"type {Encode.encode id} {Encode.encode anns} = {Encode.encode ti}"
         ""
         
-    let constraintItem (x: ConstraintItem) =
+    static member encode (x: Annotations) =
         ""
         
-    let assignItem (x: AssignItem) =
+    static member encode (x: TypeInst) =
         ""
         
-    let declareItem (x: DeclareItem) =
+    static member encode (x: Inst) =
+        match x with
+        | Inst.Var -> "var"
+        | Inst.Par -> "par"
+        | _ -> ""
+        
+    static member encode(x: BaseType) =
+        match x with
+        | Int ->
+            "int"
+        | Bool ->
+            "bool"
+        | String ->
+            "string"
+        | Float ->
+            "float"
+        | Id s ->
+            s
+        | Variable s ->
+            s
+        | Tuple fields ->
+            fields
+            |> Seq.map Encode.encode
+            |> String.concat ", "
+            |> sprintf "(%s)"
+        | Record fields ->
+            fields
+            |> Map.toSeq
+            |> Seq.map (fun (id, ti) -> $"{Encode.encode(ti)}: {id}")
+            |> String.concat ", "
+            |> sprintf "record (%s)"
+        | Literal exprs ->
+            exprs
+            |> Seq.map Encode.encode
+            |> String.concat ", "
+            |> sprintf "{%s}"
+        | Range (lo, hi) ->
+            let los = Encode.encode(lo)
+            let his = Encode.encode(hi)
+            let string = $"{los} .. {his}"
+            string
+        | List ti ->
+            $"list of {Encode.encode(ti)}"
+        | Array (dims, typ) ->            
+            let dims =
+                dims
+                |> Seq.map Encode.encode
+                |> String.concat ", "
+                |> sprintf "[%s]"                
+            let string =
+                $"array[{dims}] of {Encode.encode(typ)}"                
+            string
+        
+    static member encode(x: NumericExpr) =
+        ""
+            
+    static member encode (x: Expr) =
+        ""       
+        
+    static member encode (x: DeclareItem) =
         ""
         
-    let solveMethod (x: SolveMethod) =
+    static member encode (x: SolveMethod) =
         ""
         
-    let predicateItem (x: PredicateItem) =
+    static member encode (x: PredicateItem) =
         ""
         
-    let functionItem (x: FunctionItem) =
+    static member encode (x: FunctionItem) =
         ""
         
-    let testItem (x: TestItem) =
+    static member encode (x: CallExpr) =
         ""
-        
-    let outputItem (x: OutputItem) =
-        ""
-        
-    let annotationItem (x: AnnotationItem) =
-        ""
-        
-    let comment (x: string) =
-        x
-        
-        
-    
