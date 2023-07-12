eab879c3b7eac8f44264865e789b43cadb0cab8d
Justin Rawlings <hrkn@pm.me>
2023-07-12 23:03:18 +1000

    Adding grouping for model integration tests

--- build/ModelTests.fs
+++ build/ModelTests.fs
@@ -6,8 +6,10 @@ libminizinc test suite
 *)
 
 namespace MiniZinc
+open System.Text
 open Fake.IO
 open MiniZinc.Tests
+open Humanizer
 
 module ParserTests =
     
@@ -28,44 +30,69 @@ module ParserTests =
             |> Seq.collect (fun suite -> suite.TestCases)
             |> Seq.distinctBy (fun case -> case.TestName)
             |> Seq.toList
-        
-        let createTest (testCase: TestCase) =
             
-            let testFile =
-                testCase.TestFile.RelativeTo(LibMiniZinc.testDir)
-                    
-            let body = $"""
-        [<Fact>]
-        let ``test {testCase.TestName}`` () =
-            testParseFile @"{testFile}"
-    """
-            body
+        let testGroups =
+            testCases
+            |> List.groupBy (fun testCase -> testCase.TestFile.Directory.Name)
+            
+        let source = StringBuilder()
+        
+        let mutable tab = 0
+        
+        let indent () =
+            tab <- tab + 1
+            
+        let dedent () =
+            tab <- tab - 1
+            
+        let write (msg: string) =
+            for i in 0 .. tab do
+                source.Append "    "
+            source.AppendLine msg
+            ()
+            
+        let writeln() =
+            source.AppendLine()
+            
+        let writei msg =
+            write msg
+            indent()
                 
-        let mutable source = """namespace MiniZinc.Tests
-
-    open MiniZinc
-    open MiniZinc.Tests
-    open Xunit
-    open System.IO
-
-    module IntegrationTests =
-            
-        let testParseFile filePath =
-            let file = LibMiniZinc.testDir </> filePath
-            let result = parseModelFile file.FullName
-            match result with
-            | Result.Ok model ->
-                ()
-            | Result.Error err ->
-                Assert.Fail(err.Message)            
-    """
+        write "namespace MiniZinc.Tests"
+        write "open MiniZinc"
+        write "open MiniZinc.Tests"
+        write "open Xunit"
+        write "open System.IO"
+        writeln()
+        writei "module IntegrationTests ="
+        writeln()
+        writei "let test filePath ="
+        write "let file = LibMiniZinc.testDir </> filePath"
+        write "let result = parseModelFile file.FullName"
+        write "match result with"
+        write "| Result.Ok model -> ()"
+        write "| Result.Error err -> Assert.Fail(err.Message)"
+        dedent()
+        writeln()
         
-        for testCase in testCases do
-            let body = createTest testCase
-            source <- source + body
-           
-        source
-        
+        for group, tests in testGroups do
+            let moduleName = group.Humanize(LetterCasing.Title)
+            writei $"module ``{moduleName}`` ="
+            writeln()
+            for testCase in tests do
+                let testFile =
+                    testCase.TestFile.RelativeTo(LibMiniZinc.testDir)
+                let testName =
+                    testCase.TestName
+                write "[<Fact>]"
+                writei $"let ``test {testName}`` () ="
+                write $"test @\"{testFile}\""
+                writeln()
+                dedent()
+            dedent()
+
+        let source = string source
+                
         File.writeString
             false
             tests_file.FullName
