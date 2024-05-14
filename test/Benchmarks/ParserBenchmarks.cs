996a49d46aa6f2b9377303651932c018f0fe41aa
Justin Rawlings <justin.rawlings@protonmail.com>
2024-05-15 09:55:14 +1000

    meta // add benchmarking project

---
+++ test/Benchmarks/ParserBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿namespace Benchmarks;
+
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Attributes;
+using MiniZinc.Build;
+using MiniZinc.Parser;
+
+[MemoryDiagnoser]
+[SimpleJob(RunStrategy.Monitoring, launchCount: 1, iterationCount:1, warmupCount:1)]
+public class ParserBenchmarks
+{
+    
+    public FileInfo[] TestFiles { get; }
+    
+    public ParserBenchmarks()
+    {
+        TestFiles = Repo.TestSpecDir.JoinDir("examples").GetFiles();
+    }
+    
+    [ParamsSource(nameof(TestFiles))]
+    public FileInfo TestFile;
+
+    public string Mzn;
+    
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        Mzn = TestFile.OpenText().ReadToEnd();
+    }
+    
+    [Benchmark]
+    public void Benchmark()
+    {
+        var parser = Parser.ParseText(Mzn);
+        parser.Parse(out var tree);
+    }
+}
