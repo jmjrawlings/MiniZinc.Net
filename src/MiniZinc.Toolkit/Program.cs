5691f8d1a747b87f5c1dc37c113c3743e1831029
Justin Rawlings <justin.rawlings@protonmail.com>
2024-05-20 09:51:42 +1000

    tooolkit // Added MiniZinc.Toolkit with minify support

---
+++ src/MiniZinc.Toolkit/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace MiniZinc.Toolkit;
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.Diagnostics;
+using Parser;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var root = new RootCommand("MiniZinc Toolkit");
+
+        var minify = new Command(
+            "minify",
+            "Minify the given models, removing all comments and extraneous whitespace"
+        );
+        var format = new Command("format", "Format the given models");
+
+        var filePatternArg = new Argument<List<FileInfo>>(
+            name: "files",
+            description: "A path or pattern to minizinc files (.mzn, .dzn)",
+            parse: result =>
+            {
+                var cwd = new DirectoryInfo(Directory.GetCurrentDirectory());
+                var files = new List<FileInfo>();
+                foreach (var token in result.Tokens)
+                {
+                    if (File.Exists(token.Value))
+                    {
+                        files.Add(new FileInfo(token.Value));
+                        continue;
+                    }
+
+                    foreach (var file in cwd.EnumerateFiles(token.Value))
+                    {
+                        Console.WriteLine(file.FullName);
+                        files.Add(file);
+                    }
+                }
+                return files;
+            }
+        );
+
+        minify.AddArgument(filePatternArg);
+        minify.SetHandler(
+            async (context) =>
+            {
+                var files = context.ParseResult.GetValueForArgument(filePatternArg);
+                await Minify(context, files);
+            }
+        );
+
+        format.AddArgument(filePatternArg);
+        format.SetHandler(
+            async (context) =>
+            {
+                var files = context.ParseResult.GetValueForArgument(filePatternArg);
+                await Minify(context, files);
+            }
+        );
+
+        root.Add(minify);
+
+        var result = await root.InvokeAsync(args);
+        return result;
+    }
+
+    static void Minify(InvocationContext context, FileInfo file)
+    {
+        var console = context.Console;
+        var result = Parser.ParseFile(file);
+        if (!result.Ok)
+        {
+            console.Error.WriteLine(result.ErrorMessage!);
+            return;
+        }
+
+        var model = result.Syntax;
+        var minified = model.Write(WriteOptions.Minimal);
+        File.WriteAllText(file.FullName, minified);
+        console.WriteLine(file.FullName);
+    }
+
+    static Task Minify(InvocationContext context, List<FileInfo> files)
+    {
+        var console = context.Console;
+        var watch = Stopwatch.StartNew();
+
+        foreach (var file in files)
+            Minify(context, file);
+
+        watch.Stop();
+        console.WriteLine($"Minified {files.Count} files in {watch.ElapsedMilliseconds}ms");
+        return Task.CompletedTask;
+    }
+
+    static void Format(InvocationContext context, FileInfo file)
+    {
+        var console = context.Console;
+        var result = Parser.ParseFile(file);
+        if (!result.Ok)
+        {
+            console.Error.WriteLine(result.ErrorMessage!);
+            context.ExitCode = 100;
+            return;
+        }
+        var model = result.Syntax;
+        var output = model.Write(WriteOptions.Pretty);
+        File.WriteAllText(file.FullName, output);
+        console.WriteLine(file.FullName);
+    }
+
+    static Task Format(InvocationContext context, List<FileInfo> files)
+    {
+        var console = context.Console;
+        var watch = Stopwatch.StartNew();
+
+        foreach (var file in files)
+            Format(context, file);
+
+        watch.Stop();
+        console.WriteLine($"Formatted {files.Count} files in {watch.ElapsedMilliseconds}ms");
+        return Task.CompletedTask;
+    }
+}
