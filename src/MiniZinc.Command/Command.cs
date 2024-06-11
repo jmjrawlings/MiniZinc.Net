acc9df0ff85932b749cc60c6da86c603354ec627
Justin Rawlings <justin.rawlings@protonmail.com>
2024-06-12 09:40:41 +1000

    command // bit nicer api

--- src/MiniZinc.Command/Command.cs
+++ src/MiniZinc.Command/Command.cs
@@ -7,20 +7,29 @@ using System.Runtime.CompilerServices;
 /// </summary>
 public readonly struct Command
 {
+    /// <summary>
+    /// The executable to call, eg: "git"
+    /// </summary>
     public readonly string Exe;
 
+    /// <summary>
+    /// Arguments parsed by <see cref="Args.Parse"/>
+    /// </summary>
     public readonly Arg[] Arguments;
 
-    public Command(string exe, Arg[]? args = null)
+    /// <summary>
+    /// Directory where the command should be run
+    /// </summary>
+    public readonly string? WorkingDirectory;
+
+    public Command(string exe, Arg[]? args = null, string? workingDirectory = null)
     {
         if (string.IsNullOrEmpty(exe))
             throw new ArgumentNullException(exe);
         Exe = exe;
         Arguments = args ?? Array.Empty<Arg>();
+        WorkingDirectory = workingDirectory;
     }
-
-    public Command(string exe, IEnumerable<Arg> args)
-        : this(exe, args.ToArray()) { }
 
     public Command(string exe, params string[] args)
         : this(exe, Args.Parse(args)) { }
@@ -28,18 +37,37 @@ public readonly struct Command
     /// <summary>
     /// Create a new command with the given args added
     /// </summary>
-    /// <example>new Command("git").AddArgs("checkout","-b","develop")</example>
+    /// <example>new Command("git").Add("checkout","-b","develop")</example>
     public Command AddArgs(params string[] args) => AddArgs(Args.Parse(args));
 
     /// <summary>
-    /// Create a new command with the given args added
+    /// Create a new command with the given args appended
     /// </summary>
     public Command AddArgs(Arg[] args)
     {
-        var args_ = Args.Concat(Arguments, args);
-        var cmd = new Command(Exe, args_);
+        var arguments = Args.Concat(Arguments, args);
+        var cmd = new Command(Exe, arguments);
         return cmd;
     }
+
+    /// <summary>
+    /// Returns a new command for this Exe and the given Args
+    /// </summary>
+    /// <example>new Command("git", "-v").WithArgs("--help") // "git --help")</example>
+    public Command WithArgs(params string[] args) => AddArgs(Args.Parse(args));
+
+    /// <summary>
+    /// Returns a new command with the given working directory
+    /// </summary>
+    /// <example>new Command("ls").WithWorkingDirectory("/usr/local/bin")</example>
+    public Command WithWorkingDirectory(string path) => new Command(Exe, Arguments, path);
+
+    /// <summary>
+    /// Returns a new command with the given working directory
+    /// </summary>
+    /// <example>new Command("ls").WithWorkingDirectory("/usr/local/bin")</example>
+    public Command WithWorkingDirectory(DirectoryInfo dir) =>
+        new Command(Exe, Arguments, dir.FullName);
 
     public override string ToString()
     {
@@ -52,13 +80,12 @@ public readonly struct Command
     }
 
     public async Task<ProcessResult> Run(
-        string? workingDir = null,
         bool captureStdout = true,
         bool captureStderr = true,
         CancellationToken cancellation = default
     )
     {
-        using var proc = new CommandRunner(this, workingDir);
+        using var proc = new CommandRunner(this);
         var result = await proc.Run(
             captureStdErr: captureStderr,
             captureStdOut: captureStdout,
@@ -68,11 +95,10 @@ public readonly struct Command
     }
 
     public async IAsyncEnumerable<ProcessMessage> Watch(
-        string? workingDir = null,
         [EnumeratorCancellation] CancellationToken cancellation = default
     )
     {
-        using var proc = new CommandRunner(this, workingDir);
+        using var proc = new CommandRunner(this);
         await foreach (var msg in proc.Watch(cancellation))
             yield return msg;
     }
