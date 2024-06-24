8343d87cee1e792c9907d664a17a8c36a9430989
Justin Rawlings <justin.rawlings@protonmail.com>
2024-06-24 10:43:04 +1000

    parser // improved ParseException

---
+++ src/MiniZinc.Parser/MiniZincParseException.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniZinc.Parser;
+
+public class MiniZincParseException : Exception
+{
+    /// <summary>
+    /// A more detailed error string
+    /// </summary>
+    public readonly string Trace;
+
+    /// <summary>
+    /// The token at which the error occured
+    /// </summary>
+    public readonly Token Token;
+
+    public MiniZincParseException(string message, string trace, Token token)
+        : base(message)
+    {
+        Trace = trace;
+        Token = token;
+    }
+}
