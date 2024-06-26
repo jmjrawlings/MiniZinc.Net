d161dd48ae022e77b204a999a9473f09392b8a97
Justin Rawlings <justin.rawlings@protonmail.com>
2024-06-26 16:06:27 +1000

    misc // cleanup

--- src/MiniZinc.Parser/ParseResult.cs
+++ src/MiniZinc.Parser/ParseResult.cs
@@ -21,11 +21,24 @@ public record ParseResult
     }
 }
 
+/// <summary>
+/// Result of parsing a minizinc model file (.mzn) or string.
+/// The value of the Model field will always be present
+/// even if an error occured.  In the case of an error
+/// the model will contain all of the successfully parsed
+/// statements up until that point.
+/// </summary>
 public sealed record ModelParseResult : ParseResult
 {
     public required ModelSyntax Model { get; init; }
 }
 
+/// <summary>
+/// Result of parsing a minizinc data file (.dzn) or string.
+/// The `Data` field will always be present even if an error occured.
+/// In the case of a parser error, the DataSyntax will contain all
+/// of the successfully parsed variables up until that point.
+/// </summary>
 public sealed record DataParseResult : ParseResult
 {
     public required DataSyntax Data { get; init; }
