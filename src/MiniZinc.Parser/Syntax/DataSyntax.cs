3eedeb29bd8f9d24eb2faffe73379252c4010212
Justin Rawlings <justin.rawlings@protonmail.com>
2024-06-28 23:22:44 +1000

    parser // syntax equality

--- src/MiniZinc.Parser/Syntax/DataSyntax.cs
+++ src/MiniZinc.Parser/Syntax/DataSyntax.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Result of parsing a minizinc data from a file (.dzn) or string
 /// </summary>
-public sealed class DataSyntax
+public sealed class DataSyntax : IEquatable<DataSyntax>
 {
     public readonly IReadOnlyList<AssignmentSyntax> Assignments;
     public readonly IReadOnlyDictionary<string, ExpressionSyntax> Variables;
@@ -23,5 +23,31 @@ public sealed class DataSyntax
         writer.WriteData(this);
         var mzn = writer.ToString();
         return mzn;
+    }
+
+    public bool Equals(DataSyntax? other)
+    {
+        if (other is null)
+            return false;
+        if (ReferenceEquals(this, other))
+            return true;
+        foreach (var assign in Assignments)
+        {
+            var name = assign.Name;
+            var expr = assign.Expr;
+            if (!other.Variables.TryGetValue(name, out var b))
+                return false;
+            if (!expr.Equals(b))
+                return false;
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+            return true;
+        return Equals(obj as DataSyntax);
     }
 }
--- src/MiniZinc.Parser/Syntax/SyntaxNode.cs
+++ src/MiniZinc.Parser/Syntax/SyntaxNode.cs
@@ -1,24 +1,99 @@
 ﻿namespace MiniZinc.Parser.Syntax;
 
-public abstract class SyntaxNode
+public abstract class SyntaxNode : IEquatable<SyntaxNode>
 {
+    /// <summary>
+    /// The token at whic this node started
+    /// </summary>
     public readonly Token Start;
+
+    public List<ExpressionSyntax>? Annotations { get; set; } = null;
 
     protected SyntaxNode(in Token start)
     {
         Start = start;
     }
 
-    public List<SyntaxNode>? Annotations { get; set; } = null;
-
     public string SourceText => Write(WriteOptions.Minimal);
 
+    /// <summary>
+    /// Write this node as a minizinc string
+    /// </summary>
     public string Write(WriteOptions? options = null)
     {
         var writer = new Writer(options);
         writer.WriteNode(this);
         var mzn = writer.ToString();
         return mzn;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        return Equals(obj as SyntaxNode);
+    }
+
+    public bool Equals(SyntaxNode? other)
+    {
+        if (ReferenceEquals(null, other))
+            return false;
+        if (ReferenceEquals(this, other))
+            return true;
+        switch (this, other)
+        {
+            case (IntLiteralSyntax a, IntLiteralSyntax b):
+                return a.Value == b.Value;
+            case (BoolLiteralSyntax a, BoolLiteralSyntax b):
+                return a.Value == b.Value;
+            case (FloatLiteralSyntax a, FloatLiteralSyntax b):
+                return a.Value == b.Value;
+            case (StringLiteralSyntax a, StringLiteralSyntax b):
+                return a.Value == b.Value;
+            case (WildCardSyntax, WildCardSyntax):
+                return true;
+            case (EmptyLiteralSyntax, EmptyLiteralSyntax):
+                return true;
+            case (ConstraintSyntax a, ConstraintSyntax b):
+                return a.Expr.Equals(b.Expr);
+            case (IncludeSyntax a, IncludeSyntax b):
+                return a.Path.Equals(b.Path);
+            case (AssignmentSyntax a, AssignmentSyntax b):
+                if (!a.Identifier.Equals(b.Identifier))
+                    return false;
+                if (!a.Expr.Equals(b.Expr))
+                    return false;
+                return true;
+            case (DeclarationSyntax a, DeclarationSyntax b):
+                if (!a.Identifier.Equals(b.Identifier))
+                    return false;
+                if (!a.Type.Equals(b.Type))
+                    return false;
+                if (!object.Equals(a.Body, b.Body))
+                    return false;
+                return true;
+            case (SolveSyntax a, SolveSyntax b):
+                if (a.Method != b.Method)
+                    return false;
+                if (!object.Equals(a.Objective, b.Objective))
+                    return false;
+                return true;
+            case (TypeAliasSyntax a, TypeAliasSyntax b):
+                if (!a.Identifier.Equals(b.Identifier))
+                    return false;
+                if (!a.Type.Equals(b.Type))
+                    return false;
+                return true;
+            default:
+                // TODO - rest
+                var mzn_a = this.Write(WriteOptions.Minimal);
+                var mzn_b = other.Write(WriteOptions.Minimal);
+                if (mzn_a != mzn_b)
+                    return false;
+                return true;
+        }
     }
 
     public override string ToString() => SourceText;
